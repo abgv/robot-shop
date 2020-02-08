7c24ab81f46c9350c0a8c7924084a785f4f1a192
Admin <admin@Admins-MacBook-Pro-2.local>
2020-02-07 18:26:27 -0600

    Update to test multi building and deploy

--- mongo/users.js
+++ mongo/users.js
@@ -4,8 +4,7 @@
 db = db.getSiblingDB('users');
 db.users.insertMany([
     {name: 'user', password: 'password', email: 'user@me.com'},
-    {name: 'stan', password: 'bigbrain', email: 'stan@instana.com'},
-    {name: 'abgv', password: 'test123', email: 'abraham.garcia@digitalonus.com'}
+    {name: 'stan', password: 'bigbrain', email: 'stan@instana.com'}
 ]);
 
 // unique index on the name
