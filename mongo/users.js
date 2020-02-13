56a231ebca0fc6db0a934ad1423bc06053a82f2c
Abraham Garcia <abraham.garcia@digitalonus.com>
2020-02-12 18:46:58 -0600

    Update to test multi building

--- mongo/users.js
+++ mongo/users.js
@@ -4,7 +4,8 @@
 db = db.getSiblingDB('users');
 db.users.insertMany([
     {name: 'user', password: 'password', email: 'user@me.com'},
-    {name: 'stan', password: 'bigbrain', email: 'stan@instana.com'}
+    {name: 'stan', password: 'bigbrain', email: 'stan@instana.com'},
+    {name: 'abgv', password: 'test123', email: 'abgv@outlook.com'}
 ]);
 
 // unique index on the name
